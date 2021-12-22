65b80012de6dce3a20019d8a981ef9e236c53cbd
Bohdan-Ta <81333397+Bohdan-Ta@users.noreply.github.com>
2021-12-22 23:15:04 +0200

    rewrite App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,50 +1,37 @@
-import { Component } from 'react';
-import { ToastContainer } from 'react-toastify';
+import { useState } from "react";
+import { ToastContainer } from "react-toastify";
 
-import Sections from '../Section/Section';
-import Searchbar from '../Searchbar/Searchbar';
-import ImageGallery from '../ImageGallery';
-import Modal from '../Modal';
+import Sections from "../Section/Section";
+import Searchbar from "../Searchbar/Searchbar";
+import ImageGallery from "../ImageGallery";
+import Modal from "../Modal";
 
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
-export default class App extends Component {
-  state = {
-    showModal: false,
-    searchValue: '',
-    modalPicture: {
-      src: '',
-      alt: '',
-    },
+export default function App() {
+  const [showModal, setshowModal] = useState(false);
+  const [searchValue, setsearchValue] = useState("");
+  const [modalPicture, setmodalPicture] = useState({ src: "", alt: "" });
+
+  const getNameImage = (searchValue) => {
+    setsearchValue(searchValue);
   };
 
-  getNameImage = searchValue => {
-    this.setState({ searchValue });
-  };
-
-  toggleModal = (src, alt) => {
-    this.setState(({ showModal }) => ({
+  const toggleModal = (src, alt) => {
+    setshowModal((showModal) => ({
       showModal: !showModal,
-      modalPicture: {
-        src,
-        alt,
-      },
     }));
+    setmodalPicture({ src, alt });
   };
 
-  render() {
-    const { searchValue, showModal, modalPicture } = this.state;
-    return (
-      <div>
-        <Searchbar onSubmit={this.getNameImage} />
-        <Sections>
-          <ImageGallery imageName={searchValue} openModal={this.toggleModal} />
-        </Sections>
-        {showModal && (
-          <Modal onClose={this.toggleModal} modalPicture={modalPicture} />
-        )}
-        <ToastContainer autoClose={3000} />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Searchbar onSubmit={getNameImage} />
+      <Sections>
+        <ImageGallery imageName={searchValue} openModal={toggleModal} />
+      </Sections>
+      {showModal && <Modal onClose={toggleModal} modalPicture={modalPicture} />}
+      <ToastContainer autoClose={3000} />
+    </div>
+  );
 }
